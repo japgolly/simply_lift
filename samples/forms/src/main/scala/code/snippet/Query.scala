58cc5369f80cc0f378fb7c0a1c83085db83a2353
David Barri <japgolly@gmail.com>
2013-04-26 14:32:59 +1000

    Fixed indenting and aligned comments in Query.scala

--- samples/forms/src/main/scala/code/snippet/Query.scala
+++ samples/forms/src/main/scala/code/snippet/Query.scala
@@ -8,11 +8,10 @@ import Helpers._
 
 object Query {
   def results = ClearClearable andThen
-  "li *" #> S.param("q"). // get the query parameter
-  toList. // convert the Box to a List
-  flatMap(q => {
-    ("You asked: "+q) :: // prepend the query
-    (1 to toInt(q)).toList.map(_.toString) // if it can be converted to an Int
-    // convert it and return a sequence of Ints
-  })
+    "li *" #> S.param("q").                    // get the query parameter
+      toList.                                  // convert the Box to a List
+      flatMap(q => {
+        ("You asked: "+q) ::                   // prepend the query
+        (1 to toInt(q)).toList.map(_.toString) // if it can be converted to an Int
+      })                                       // convert it and return a sequence of Ints
 }
